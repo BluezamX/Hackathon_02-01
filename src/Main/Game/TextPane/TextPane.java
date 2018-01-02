566c5d30a45d1cd066b9427329f639cd621019d5
Yvan Bibollet-Ruche <yvan_1999@hotmail.nl>
2018-01-02 14:25:15 +0100

    JEMOEDERISEENHOER

--- src/Main/Game/TextPane/TextPane.java
+++ src/Main/Game/TextPane/TextPane.java
@@ -1,9 +1,26 @@
 package Main.Game.TextPane;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+
 
 /**
  * Created by jaspe on 02/01/2018.
  */
 public class TextPane extends Pane {
+
+    public TextPane(){
+        TextArea area = new TextArea();
+        area.setEditable(false);
+        area.setText("JEMOEDERISEENGROOTEHOERMETEENDIKKENEUS");
+        getChildren().add(area);
+
+    }
+
 }
